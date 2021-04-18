8c7a0f1329076b3cc2d6574891011c02c45ea5f1
Tobias B <germansuperioz@yahoo.de>
2021-04-18 12:32:47 +0200

    Add other subcommands

--- indigo-client-spigot/src/main/java/network/cow/indigo/client/spigot/command/RolesCommand.kt
+++ indigo-client-spigot/src/main/java/network/cow/indigo/client/spigot/command/RolesCommand.kt
@@ -2,10 +2,13 @@ package network.cow.indigo.client.spigot.command
 
 import io.grpc.Status
 import network.cow.indigo.client.spigot.IndigoPlugin
+import network.cow.mooapis.indigo.v1.AddRolePermissionRequest
 import network.cow.mooapis.indigo.v1.DeleteRoleRequest
 import network.cow.mooapis.indigo.v1.GetRoleRequest
+import network.cow.mooapis.indigo.v1.GetUserRolesRequest
 import network.cow.mooapis.indigo.v1.InsertRoleRequest
 import network.cow.mooapis.indigo.v1.ListRolesRequest
+import network.cow.mooapis.indigo.v1.RemoveRolePermissionRequest
 import network.cow.mooapis.indigo.v1.Role
 import org.bukkit.Bukkit
 import org.bukkit.command.Command
@@ -27,7 +30,13 @@ class RolesCommand(private val plugin: IndigoPlugin) : CommandExecutor, TabCompl
         Bukkit.getScheduler().runTaskAsynchronously(plugin, object : Runnable {
             override fun run() {
                 when (args[0]) {
-                    "list" -> list(sender)
+                    "list" -> {
+                        if (args.size == 1) {
+                            list(sender, null)
+                        } else {
+                            list(sender, args[1])
+                        }
+                    }
                     "info" -> {
                         if (args.size == 1) {
                             sender.sendMessage("§c/roles info <name>")
@@ -48,6 +57,16 @@ class RolesCommand(private val plugin: IndigoPlugin) : CommandExecutor, TabCompl
                             return
                         }
                         delete(sender, args[1])
+                    }
+                    "permission" -> {
+                        if (args.size == 1) {
+                            sender.sendMessage("§cAvailable sub commands:")
+                            sender.sendMessage("§7- /roles permission list <name>")
+                            sender.sendMessage("§7- /roles permission add <name> <permission>")
+                            sender.sendMessage("§7- /roles permission remove <name> <permission>")
+                            return
+                        }
+                        permission(sender, args.slice(1..args.size))
                     }
                     else -> sendUsage(sender)
                 }
@@ -80,7 +99,6 @@ class RolesCommand(private val plugin: IndigoPlugin) : CommandExecutor, TabCompl
     }
 
     private fun sendUsage(sender: CommandSender) {
-        // TODO
         sender.sendMessage("§cAvailable commands:")
         sender.sendMessage("§7- /roles list [player]")
         sender.sendMessage("§7- /roles info <name>")
@@ -93,7 +111,34 @@ class RolesCommand(private val plugin: IndigoPlugin) : CommandExecutor, TabCompl
         sender.sendMessage("§7- /roles unassign <player> <role>")
     }
 
-    private fun list(sender: CommandSender) {
+    private fun list(sender: CommandSender, player: String?) {
+        if (player != null) {
+            try {
+                val response = plugin.blockingStub.getUserRoles(GetUserRolesRequest.newBuilder().setUserAccountId(player).build())
+
+                sender.sendMessage("§aUser has roles:")
+                response.rolesList.forEach {
+                    sender.sendMessage("§7- §f${it.id} (color: ${it.color})")
+                }
+            } catch (ex: Exception) {
+                val status = Status.fromThrowable(ex)
+
+                when (status.code) {
+                    Status.Code.NOT_FOUND -> {
+                        sender.sendMessage("§cUser does not have any roles.")
+                    }
+                    Status.Code.UNAVAILABLE -> {
+                        sender.sendMessage("§cThe service is currently offline. Please try again later.")
+                    }
+                    else -> {
+                        sender.sendMessage("§4There have been an error during the request. Please look into the log.")
+                        ex.printStackTrace()
+                    }
+                }
+            }
+            return
+        }
+
         try {
             val response = plugin.blockingStub.listRoles(ListRolesRequest.newBuilder().build())
 
@@ -185,6 +230,115 @@ class RolesCommand(private val plugin: IndigoPlugin) : CommandExecutor, TabCompl
             )
 
             sender.sendMessage("§aRole $name removed.")
+        } catch (ex: Exception) {
+            val status = Status.fromThrowable(ex)
+
+            when (status.code) {
+                Status.Code.NOT_FOUND -> {
+                    sender.sendMessage("§cRole does not exist.")
+                }
+                Status.Code.UNAVAILABLE -> {
+                    sender.sendMessage("§cThe service is currently offline. Please try again later.")
+                }
+                else -> {
+                    sender.sendMessage("§4There have been an error during the request. Please look into the log.")
+                    ex.printStackTrace()
+                }
+            }
+        }
+    }
+
+    private fun permission(sender: CommandSender, args: List<String>) {
+        when (args[0]) {
+            "list" -> {
+                if (args.size == 1) {
+                    sender.sendMessage("§c/roles permission list <name>")
+                    return
+                }
+                this.permissionList(sender, args[1])
+            }
+            "add" -> {
+                if (args.size <= 2) {
+                    sender.sendMessage("§c/roles permission add <name> <permission>")
+                    return
+                }
+                this.permissionAdd(sender, args[1], args[2])
+            }
+            "remove" -> {
+                if (args.size <= 2) {
+                    sender.sendMessage("§c/roles permission remove <name> <permission>")
+                    return
+                }
+                this.permissionRemove(sender, args[1], args[2])
+            }
+        }
+    }
+
+    private fun permissionList(sender: CommandSender, name: String) {
+        try {
+            val response = plugin.blockingStub.getRole(GetRoleRequest.newBuilder().setRoleId(name).build())
+
+            val role = response.role
+            sender.sendMessage("§aPermission list of $role:")
+            role.permissionsList.forEach {
+                sender.sendMessage("§7- $it")
+            }
+        } catch (ex: Exception) {
+            val status = Status.fromThrowable(ex)
+
+            when (status.code) {
+                Status.Code.NOT_FOUND -> {
+                    sender.sendMessage("§cRole does not exist.")
+                }
+                Status.Code.UNAVAILABLE -> {
+                    sender.sendMessage("§cThe service is currently offline. Please try again later.")
+                }
+                else -> {
+                    sender.sendMessage("§4There have been an error during the request. Please look into the log.")
+                    ex.printStackTrace()
+                }
+            }
+        }
+    }
+
+    private fun permissionAdd(sender: CommandSender, name: String, permission: String) {
+        try {
+            val response = plugin.blockingStub.addRolePermission(
+                AddRolePermissionRequest.newBuilder()
+                    .setRoleId(name)
+                    .addPermissions(permission)
+                    .build()
+            )
+
+            sender.sendMessage("§aPermission added.")
+        } catch (ex: Exception) {
+            val status = Status.fromThrowable(ex)
+
+            when (status.code) {
+                Status.Code.NOT_FOUND -> {
+                    sender.sendMessage("§cRole does not exist.")
+                }
+                Status.Code.UNAVAILABLE -> {
+                    sender.sendMessage("§cThe service is currently offline. Please try again later.")
+                }
+                else -> {
+                    sender.sendMessage("§4There have been an error during the request. Please look into the log.")
+                    ex.printStackTrace()
+                }
+            }
+        }
+    }
+
+    private fun permissionRemove(sender: CommandSender, name: String, permission: String) {
+        try {
+            val response = plugin.blockingStub.removeRolePermission(
+                RemoveRolePermissionRequest.newBuilder()
+                    .setRoleId(name)
+                    .addPermissions(permission)
+                    .build()
+            )
+
+            sender.sendMessage("§aPermission removed.")
         } catch (ex: Exception) {
             val status = Status.fromThrowable(ex)
 
