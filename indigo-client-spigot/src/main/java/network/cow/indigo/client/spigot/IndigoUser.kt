e2ac85baa9f3a52952496c69e475ad5ea7624c52
Tobayy <tobiasbueser@yahoo.de>
2021-04-25 17:59:32 +0200

    Implement layer system to roles

---
+++ indigo-client-spigot/src/main/java/network/cow/indigo/client/spigot/IndigoUser.kt
@@ -0,0 +1,53 @@
+package network.cow.indigo.client.spigot
+
+import network.cow.indigo.client.spigot.permission.PermissionList
+import network.cow.mooapis.indigo.v1.Role
+import network.cow.mooapis.indigo.v1.User
+
+/**
+ * @author Tobias Büser
+ */
+class IndigoUser(private val user: User) {
+
+    val permissions: PermissionList
+
+    init {
+        var prevPriority = 0
+        var permissionList = PermissionList(listOf())
+        this.user.rolesList.forEach { role ->
+            val rolePermissionList = PermissionList(role.permissionsList)
+            val superior = role.priority > prevPriority
+
+            permissionList = permissionList.mergeWith(rolePermissionList, superior)
+
+            prevPriority = role.priority
+        }
+
+        val customPermissionList = PermissionList(user.customPermissionsList)
+        permissionList = permissionList.mergeWith(customPermissionList, true)
+
+        this.permissions = permissionList.compacted()
+    }
+
+    /**
+     * Returns the [Role] that is not [Role.getTransient] and has the highest
+     * [Role.getPriority].
+     *
+     * If the list of roles is empty, return null.
+     * And if two or more roles have the same priority, we just return the
+     * first occuring one, handled by [maxByOrNull].
+     */
+    fun getTopRole(): Role? {
+        if (user.rolesList.isEmpty()) {
+            return null
+        }
+        return user.rolesList.filter { !it.transient }.maxByOrNull { it.priority }
+    }
+
+    /**
+     * Returns a list of all [Role]s that are [Role.getTransient], sorted by
+     * their priority descending.
+     */
+    fun getTransientRoles() = user.rolesList.filter { it.transient }.sortedByDescending { it.priority }
+
+}
--- indigo-client-spigot/src/main/java/network/cow/indigo/client/spigot/permission/PermissionList.kt
+++ indigo-client-spigot/src/main/java/network/cow/indigo/client/spigot/permission/PermissionList.kt
@@ -1,21 +1,53 @@
 package network.cow.indigo.client.spigot.permission
 
+private val PERM_REGEX = Regex("^-?(?:\\w+|\\*)(?:\\.(\\w+|\\*))*\$")
+
 /**
  * @author Tobias Büser
  */
-class PermissionList(permissions: List<String>) {
+class PermissionList(permissions: List<String>) : Iterable<String> {
 
     private val regexes = mutableMapOf<String, Regex>()
+
+    private val positivePermissions: List<String>
+    private val negativePermissions: List<String>
+
+    private val size: Int
+        get() = regexes.size
 
     init {
         permissions.forEach {
             regexes[it] = Regex(it.toPermissionRegexString())
         }
+
+        positivePermissions = regexes.keys.filter { !it.isNegative }.sortedWith(compareBy(
+            { it.depth }, { it.wildcardCount * -1 }
+        ))
+        negativePermissions = regexes.keys.filter { it.isNegative }.sortedWith(compareBy(
+            { it.depth }, { it.wildcardCount * -1 }
+        ))
     }
+
+    fun negatesPermission(perm: String) = hasPermissionRaw("-$perm", true)
 
     fun hasPermission(perm: String): Boolean {
         if (regexes.containsKey(perm)) return true
-        for (regex in regexes.values) {
+
+        // first check for negative permissions
+        if (negatesPermission(perm)) return false
+
+        return hasPermissionRaw(perm)
+    }
+
+    /**
+     * Checks if the list has a [Regex] that matches the literal [perm].
+     */
+    fun hasPermissionRaw(perm: String, negative: Boolean = false): Boolean {
+        if (regexes.containsKey(perm)) return true
+        val permissionsToCheck = if (negative) negativePermissions else positivePermissions
+
+        for (permissionKey in permissionsToCheck) {
+            val regex = regexes[permissionKey] ?: continue
             if (regex.matches(perm)) {
                 return true
             }
@@ -23,13 +55,79 @@ class PermissionList(permissions: List<String>) {
         return false
     }
 
+    /**
+     * Compacts the permissions by checking if one permission
+     * either negates or is more general than other permissions.
+     * In short: Remove all unnecessary permissions.
+     */
+    fun compacted() = compacted(0)
+
+    private fun compacted(byIndex: Int): PermissionList {
+        if (this.size == 0 || byIndex >= this.size) return this
+        val copy = this.toMutableList()
+        val elem = copy[byIndex]
+        val elemPermissionList = PermissionList(listOf(elem))
+
+        copy.removeIf {
+            if (it == elem) return@removeIf false
+            if (it.isNegative) {
+                return@removeIf elemPermissionList.hasPermissionRaw(it, true)
+            }
+            return@removeIf elemPermissionList.hasPermission(it) || elemPermissionList.negatesPermission(it)
+        }
+        return PermissionList(copy).compacted(byIndex + 1)
+    }
+
+    fun mergeWith(other: PermissionList, superior: Boolean = false): PermissionList {
+        if (!superior) {
+            return PermissionList(this + other)
+        }
+
+        val mergedList = this.toMutableList()
+
+        mergedList.removeIf {
+            if (it.isNegative) {
+                return@removeIf other.hasPermission(it.drop(1))
+            }
+            return@removeIf other.negatesPermission(it)
+        }
+
+        mergedList += other
+        return PermissionList(mergedList)
+    }
+
+    /**
+     * Sorted iterator.
+     */
+    override fun iterator(): Iterator<String> {
+        return (negativePermissions + positivePermissions).iterator()
+    }
+
+    override fun toString(): String {
+        return regexes.keys.toString()
+    }
+
 }
 
-private val PERM_REGEX = Regex("^-?(?:\\w+|\\*)(?:\\.(\\w+|\\*))*\$")
+private val String.isNegative: Boolean
+    get() = this.startsWith("-")
+
+private val String.depth: Int
+    get() = this.count { it == '.' }
+
+private val String.wildcardCount: Int
+    get() = this.count { it == '*' }
 
 private fun String.toPermissionRegexString(): String {
     if (!this.isPermission) {
         return this
+    }
+    if (this.isNegative) {
+        return "-" + this.drop(1).toPermissionRegexString()
+    }
+
+    if (this == "*") {
+        return "(?:\\w+|\\*)" + "(?:\\.(\\w+|\\*))+"
     }
 
     val regexString: String
